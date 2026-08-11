dd4e2b295e97144027521092ee2177815ead9689
BuildTools <unconfigured@null.spigotmc.org>
2026-08-11 13:58:56 +0300

    feat: add redux

--- src/services/store.js
+++ src/services/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { usersApi } from './usersApi';
+import usersReducer from './usersSlice';
 
 export const store = configureStore({
   reducer: {
     [usersApi.reducerPath]: usersApi.reducer,
+    users: usersReducer,
   },
 
   middleware: (getDefaultMiddleware) =>
