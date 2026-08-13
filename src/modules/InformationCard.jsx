3cbd3cc8a6781aed4caa5d7dfbb71b63738b4bc8
BuildTools <unconfigured@null.spigotmc.org>
2026-08-14 00:49:41 +0300

    feat: add canceling for editing in information card

--- src/modules/InformationCard.jsx
+++ src/modules/InformationCard.jsx
@@ -1,61 +1,85 @@
-
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { updateUser } from "../services/usersSlice";
 import { useSelector } from 'react-redux';
+import { updateUser } from '../services/usersSlice';
 
-export default function InformationCard( {info} ) { 
+export default function InformationCard({ info }) {
     const dispatch = useDispatch();
-    const [isEditing, setIsEditing] = useState(false); 
-    const fieldsRef = useRef({});
 
-    const user = useSelector(
-    (state) => state.users.users.find(
-        (user) => user.id === 1
-    )
-  );
+    const [isEditing, setIsEditing] = useState(false);
 
+    const [fields, setFields] = useState(() => {
+        const initialFields = {};
+
+        Object.entries(info.fields).forEach(([key, field]) => {
+            initialFields[key] = field.value;
+        });
+
+        return initialFields;
+    });
+
+    const [previousFields, setPreviousFields] = useState({});
 
     const handleEdit = () => {
+        setPreviousFields({ ...fields });
         setIsEditing(true);
     };
 
+    const handleCancel = () => {
+        setFields(previousFields);
+        setIsEditing(false);
+    };
+
     const handleSave = () => {
-            const changes = {};
-
-            Object.entries(fieldsRef.current).forEach(([key, element]) => {
-                if (element) {
-                    changes[key] = element.innerText;
-                }
-            });
-
-            dispatch(
-                updateUser({
-                    id: 1,
-                    changes
-                })
-            );
+        dispatch(
+            updateUser({
+                id: 1,
+                changes: fields
+            })
+        );
 
         setIsEditing(false);
     };
 
     return (
-                    <div className='card'>
-                        <div className='label'>
-                            <h2>{info.category}</h2>
-                            { info.isEditable ? <button className='action' onClick={isEditing ? handleSave : handleEdit}><img src="buttons/edit.png" alt="Redact" /></button> : null }
-                        </div>
+        <div className="card">
+            <div className="label">
+                <h2>{info.category}</h2>
 
-                        <hr />
+                {info.isEditable
+                    ? isEditing
+                        ? (
+                            <>
+                                <button className="action" onClick={handleCancel}><img src="buttons/cancel.png" alt="Cancel"/></button>
+                                <button className="action" onClick={handleSave}><img src="buttons/confirm.png" alt="Confirm"/></button>
+                            </>
+                        )
+                        : (
+                            <button className="action" onClick={handleEdit}><img src="buttons/edit.png" alt="Redact" /></button>
+                        )
+                    : null}
+            </div>
 
-                        <dl>
-                            {Object.entries(info.fields).map(([key, field]) => (
-                                <React.Fragment key={key}>
-                                    <dt>{field.name}</dt>
-                                    <dd contentEditable={isEditing} ref={(el) => fieldsRef.current[key] = el}>{field.value}</dd>
-                                </React.Fragment>
-                            ))}
-                        </dl>
-                    </div>
-    )
+            <hr />
+
+            <dl>
+                {Object.entries(info.fields).map(([key, field]) => (
+                    <React.Fragment key={key}>
+                        <dt>{field.name}</dt>
+
+                        <dd>
+                            {isEditing ? ( <input value={fields[key]} onChange={(e) => {
+                                        setFields((prev) => ({
+                                            ...prev,
+                                            [key]: e.target.value
+                                        }));
+                                    }}
+                                />
+                            ) : fields[key]}
+                        </dd>
+                    </React.Fragment>
+                ))}
+            </dl>
+        </div>
+    );
 }
