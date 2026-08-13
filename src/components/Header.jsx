03cd5e3a1896a81bed23e85bdacaf0410798a8b9
BuildTools <unconfigured@null.spigotmc.org>
2026-08-14 02:43:00 +0300

    feat: add burger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Header() {
+  const [burger, setBurger] = useState(false);
+
   return (
+    <>
         <header className='menu'>
             <div className='navigation'>
-            <img src="buttons/hamburger.png" alt="hamburger" />
+                <button><img src="buttons/hamburger.png" alt="hamburger" onClick={() => setBurger(true)}/></button>
 
-            <div>
-                <img src="other/hcare.jpg" alt="HCare logo" />
-                <h2>HCare</h2>
-            </div>
+                <div>
+                    <img src="other/hcare.jpg" alt="HCare logo" />
+                    <h2>HCare</h2>
+                </div>
 
             </div>
 
@@ -16,5 +22,16 @@ export default function Header() {
             <img src="other/avatar.jpg" alt="Avatar" />
             </div>
       </header>
+
+      { burger ? <nav className='burger'>
+        <button className='action'><img src="buttons/close_burger.png" alt="Close" onClick={() => setBurger(false)}/></button>
+        <ul>
+            <li><Link to="/" className='link'>Profile</Link></li>
+            <li><Link to="/doctors" className='link'>Medical Staff</Link></li>
+            <li><Link to="/feedback" className='link'>Feedback</Link></li>
+        </ul>
+      </nav>
+      : null }
+    </>
   )
 }
