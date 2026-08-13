953294327d67b667c5893a1436146d3849bed62a
BuildTools <unconfigured@null.spigotmc.org>
2026-08-14 01:38:57 +0300

    feat: add error notification

--- src/modules/PopUp.jsx
+++ src/modules/PopUp.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { updateUser } from "../services/usersSlice";
 
 export default function PopUp( {addition, onClose, list} ) {
     const dispatch = useDispatch();
+    const errorInput = useRef({});
 
     const [update, setUpdate] = useState({
         ...Object.keys(addition.inputs).map((key) => {
@@ -32,10 +33,10 @@ export default function PopUp( {addition, onClose, list} ) {
     const handleSubmit = (e) => {
             e.preventDefault();
             
-            
-
-            for (const input of Object.values(update)) {
+            for (const [field, input] of Object.entries(update)) {
                 if (input === "") {
+                    errorInput.current.setCustomValidity(`The field ${field} is not filled in`);
+                    errorInput.current.reportValidity();
                     return;
                 }
             } 
@@ -58,48 +59,59 @@ export default function PopUp( {addition, onClose, list} ) {
         }
 
     return (
-    <form>
-        <button className='action close' onClick={() => onClose(false)}><img src="buttons/close.png" alt="Close" /></button>
-        
-         {Object.entries(addition.inputs).map(([key, input]) => {
-            if (input.type == "checkbox") {
-                return (
-                    <label key={key}>
-                        <h4>{input.name}</h4>
-                        <input type="checkbox" checked={update[key]} onChange={(e) => setUpdate( {...update, [key]: e.target.checked} )}/>
-                    </label>
-                )
-            }
+        <>
+            <div className="backdrop"></div>
+            <form style={
+                {
+                    height: 100 + (100 * Object.keys(addition.inputs).length)
+                }
+            }>
+                <button className='action close' onClick={() => onClose(false)}><img src="buttons/close.png" alt="Close" /></button>
+                
+                {Object.entries(addition.inputs).map(([key, input]) => {
+                    if (input.type == "checkbox") {
+                        return (
+                            <label key={key} className="check">
+                                <h4>{input.name}</h4>
+                                <input type="checkbox" checked={update[key]} onChange={(e) => setUpdate( {...update, [key]: e.target.checked} )}/>
+                            </label>
+                        )
+                    }
 
-            else if (input.type == "select") {
-                return (
-                    <label key={key}>
-                        <h4>{input.name}</h4>
-                        <select onChange={(e) => setUpdate( {...update, [key]: e.target.value} ) } value={update[key]}>
-                            {
-                                Object.entries(input.options).map(([key, option]) => (
-                                    <option value={key} key={key}>{option}</option>
-                                ))
-                            }
-                        </select>
-                    </label>
-                )
-            }
+                    else if (input.type == "select") {
+                        return (
+                            <label key={key}>
+                                <h4>{input.name}</h4>
+                                <select onChange={(e) => setUpdate( {...update, [key]: e.target.value} ) } value={update[key]}>
+                                    {
+                                        Object.entries(input.options).map(([key, option]) => (
+                                            <option value={key} key={key}>{option}</option>
+                                        ))
+                                    }
+                                </select>
+                            </label>
+                        )
+                    }
 
-            else {
-                return (
-                <label key={key}>
-                    <h4>{input.name}</h4>
-                    <input type={input.type} className={key} onChange={(e) => setUpdate( {...update, [key]: e.target.value} ) } value={update[key]}/>
-                </label>
-                )
-            }
-        }
-    )}
+                    else {
+                        return (
+                        <label key={key}>
+                            <h4>{input.name}</h4>
+                            <input type={input.type} className={key} onChange={(e) => setUpdate( {...update, [key]: e.target.value} ) } value={update[key]}/>
+                        </label>
+                        )
+                    }
+                }
+            )}
 
-    <div>
-        <button className='action' onClick={handleSubmit}><img src="buttons/send.png" alt="Send" /></button>
-    </div>
-    </form>
+                <input type="text" className="error" ref={errorInput}/>
+
+                <div>
+                    <button className='action send' onClick={handleSubmit}><img src="buttons/send.png" alt="Send" /></button>
+                </div>
+
+                
+            </form>
+    </>
     )
 }
