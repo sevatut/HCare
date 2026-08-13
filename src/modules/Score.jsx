f43b348c4cf5f98f2dfd888871e7ff84fd24aefd
BuildTools <unconfigured@null.spigotmc.org>
2026-08-13 11:18:22 +0300

    feat: divide function in another file

--- src/modules/Score.jsx
+++ src/modules/Score.jsx
@@ -1,18 +1,4 @@
-function defineGrade(number) {
-
-    switch (true) {
-        case number >= 1 && number <= 3:
-            return "bad";
-        case number >= 4 && number <= 6:
-            return "normal";
-        case number >= 7 && number <= 9:
-            return "good";
-        case number == 10:
-            return "excellent";
-        default: 
-            return "bad";
-    }
-} 
+import { defineGrade } from "../utls/definder";
 
 export default function Score( {title} ) {
   return (
